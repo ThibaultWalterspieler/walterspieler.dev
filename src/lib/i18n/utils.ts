2edd0c81cec7356555a77968fce2ee8ee4534e84
Thibault Walterspieler <twalterspieler@pm.me>
2024-12-31 14:19:29 +0100

    chore: remove negotiator dependency and update lexical packages
    - Removed the negotiator package from dependencies and its related type definitions.
    - Updated lexical packages from version 0.22.0 to 0.20.0
    - Simplified locale handling in utils.ts by removing the getLocaleFromRequest function.

--- src/lib/i18n/utils.ts
+++ src/lib/i18n/utils.ts
@@ -1,9 +1,5 @@
-import { match } from "@formatjs/intl-localematcher";
-import Negotiator from "negotiator";
-import { NextRequest } from "next/server";
 import { TypedLocale } from "payload";
 
-import { I18N_CONFIG } from "./config";
 import { DICTIONARIES } from "./constants";
 
 const getDictionary = (locale: TypedLocale) => {
@@ -14,17 +10,4 @@ const getDictionary = (locale: TypedLocale) => {
   return DICTIONARIES[locale]();
 };
 
-const getLocaleFromRequest = (request: NextRequest): string | undefined => {
-  const negotiatorHeaders: Record<string, string> = {};
-  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
-
-  const locales = I18N_CONFIG.locales;
-
-  const languages = new Negotiator({ headers: negotiatorHeaders }).languages(
-    locales as unknown as string[],
-  );
-
-  return match(languages, locales, I18N_CONFIG.defaultLocale);
-};
-
-export { getDictionary, getLocaleFromRequest };
+export { getDictionary };
