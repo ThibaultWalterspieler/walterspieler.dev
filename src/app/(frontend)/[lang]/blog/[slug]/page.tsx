c6580c9b7923cc1120c2fe31ebd3d2c86437eae5
Thibault Walterspieler <twalterspieler@pm.me>
2025-01-04 15:29:25 +0100

    feat: enhance blog post handling with caching and revalidation

--- src/app/(frontend)/[lang]/blog/[slug]/page.tsx
+++ src/app/(frontend)/[lang]/blog/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { cache, FC } from "react";
 
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
@@ -9,7 +9,7 @@ import ArticleBreadcrumb from "@/components/Articles/ArticleBreadcrumb";
 import ScrollArea from "@/components/Common/ScrollArea";
 import getSchemaNewsArticle from "@/lib/schema-dts/news-article";
 import getMetadata from "@/lib/seo/metadata";
-import config from "@payload-config";
+import { default as configPromise } from "@payload-config";
 
 type Params = Promise<{
   lang: TypedLocale;
@@ -20,27 +20,20 @@ type Props = {
   params: Params;
 };
 
-const getBlogPost = async (slug: string, lang: TypedLocale) => {
-  const payload = await getPayload({
-    config,
-  });
-
+export async function generateStaticParams() {
+  const payload = await getPayload({ config: configPromise });
   const blogPosts = await payload.find({
     collection: "blogPosts",
-    locale: lang,
-    where: {
-      slug: {
-        equals: slug,
-      },
+    limit: 1000,
+    select: {
+      slug: true,
     },
   });
 
-  if (!blogPosts.docs[0]) {
-    notFound();
-  }
-
-  return blogPosts.docs[0];
-};
+  return blogPosts.docs.map((blogPost) => ({
+    slug: blogPost.slug,
+  }));
+}
 
 const BlogPostPage: FC<Props> = async (props) => {
   const { params } = props;
@@ -80,6 +73,28 @@ const BlogPostPage: FC<Props> = async (props) => {
     </>
   );
 };
+
+const getBlogPost = cache(async (slug: string, lang: TypedLocale) => {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+
+  const blogPosts = await payload.find({
+    collection: "blogPosts",
+    locale: lang,
+    where: {
+      slug: {
+        equals: slug,
+      },
+    },
+  });
+
+  if (!blogPosts.docs[0]) {
+    notFound();
+  }
+
+  return blogPosts.docs[0];
+});
 
 export async function generateMetadata(props: Props): Promise<Metadata> {
   const { params } = props;
